e8a43528dcdddde821f4e6657eb744340cac8fec
Mario Cagalj <mcagalj@fesb.hr>
2024-11-20 11:42:14 +0100

    Add primary key to pages table schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,7 @@
-import { pgTable, text, boolean } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, boolean } from 'drizzle-orm/pg-core';
 
 export const pages = pgTable('pages', {
+    id: serial('id').primaryKey(),
     title: text('title').notNull().unique(),
     path: text('path').notNull().unique(),
     includeInProd: boolean('include_in_prod').notNull().default(false),
