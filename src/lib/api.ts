3dd735611fe9911e0183217bd62e932c3b20d699
Mario Cagalj <mcagalj@fesb.hr>
2024-11-25 11:11:49 +0100

    feat: Add Contentful product fetching and create products page with image support

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,15 +4,27 @@ import { db } from "@/db/drizzle";
 import { pages } from "@/db/schema";
 import contentfulClient from "@/lib/contentfulClient";
 import { unstable_cache } from "next/cache"; // Replaced by 'use cache' directive in new version of Next.js
-import { TypeNavigationSkeleton } from '@/content-types';
+import { TypeNavigationSkeleton, TypeProductSkeleton } from '@/content-types';
 
+const MINUTE = 60;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+
+// ===============================
+// Fetching data from a database
+// ===============================
 async function getPages() {
     const data = await db.select().from(pages).orderBy(pages.order);
     return data;
 }
+export const getNavigation = unstable_cache(getPages, ['pages'], { revalidate: DAY, tags: ['pages'] });
 
-export const getNavigation = unstable_cache(getPages, ['pages'], { revalidate: 60 * 60, tags: ['pages'] });
 
+// =====================================================================================
+// Fetching data from Contentful (a headless CMS) using the 'contentful.js' library.
+// This library is a wrapper around Contentful Delivery REST API.
+// (https://github.com/contentful/contentful.js)
+// =====================================================================================
 export const getMainNavigation = unstable_cache(async () => {
     // Check https://github.com/contentful/contentful.js/blob/master/ADVANCED.md#link-resolution
     // for more information on "withoutUnresolvableLinks".
@@ -24,4 +36,13 @@ export const getMainNavigation = unstable_cache(async () => {
 
     return data.items[0];
 
-}, ['navigation'], { revalidate: 60 * 60, tags: ['navigation'] });  
+}, ['navigation'], { revalidate: HOUR, tags: ['navigation'] });
+
+
+export const getProducts = unstable_cache(async () => {
+    const data = await contentfulClient.withoutUnresolvableLinks.getEntries<TypeProductSkeleton>({
+        content_type: 'product',
+    });
+
+    return data;
+}, ['products'], { revalidate: HOUR, tags: ['products'] });
