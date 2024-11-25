fb1de5655f4c934fe18c76edcfeb285964c5c7ef
Mario Cagalj <mcagalj@fesb.hr>
2024-11-25 12:21:30 +0100

    feat: Add pagination component for products page; implemented as a React client component (in contrast to Blog pagination component - sever component)

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { getProducts, getProductsCount } from "@/lib/api";
 import type { Metadata } from "next";
+import Pagination from "./_components/pagination";
 
 export const metadata: Metadata = {
   title: "Products",
@@ -9,6 +10,7 @@ export const metadata: Metadata = {
 type ProductsPageProps = {
   searchParams: { page: string };
 };
+
 const PAGE_SIZE = Number(process.env.PRODUCTS_PAGE_SIZE) || 6;
 
 export default async function ProductsPage({
@@ -22,12 +24,13 @@ export default async function ProductsPage({
     pagesCount
   );
   const skip = (currentPage - 1) * PAGE_SIZE;
-  console.log({ pagesCount, currentPage, skip });
+
   const data = await getProducts({ skip });
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-10">
+    <main className="flex min-h-screen flex-col items-center p-10 space-y-12">
       <h1 className="text-6xl font-extrabold tracking-tight">Products</h1>
+      <Pagination page={currentPage} totalPages={pagesCount} />
       <ul className="w-full space-y-4">
         {data.items.map((product) => (
           <li key={product.sys.id}>
